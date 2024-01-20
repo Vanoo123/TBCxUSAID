73f3c2de7767afb4de673bf5b410dd58599d2a63
Ivane Tvildiani <vanotvildiani7@gmail.com>
2024-01-20 21:34:48 +0400

    Added hamburger menu and mobile navbar

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,20 @@ document.addEventListener("scroll", () => {
     header.classList.remove("scrolled");
   }
 });
+
+// For Burger Rotation & opening menu
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const mobile = document.getElementById("mobile");
+  const blur = document.getElementById("blur");
+
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("openmenu");
+    mobile.classList.toggle("show");
+    mobile.classList.toggle("hide");
+    blur.classList.toggle("show2");
+    blur.classList.toggle("hide2");
+    burger.classList.toggle("gray_button");
+    document.body.classList.toggle("overflow_hidden");
+  });
+});
